7282525f2a0387088eaf111f36e746434fc70954
DESKTOP-GTC07B0\Sithum <sithumdilshan985@gmail.com>
2023-05-26 10:40:18 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  async function handleClick(i, j) {
+    const data = {
+      key:i,
+      sec:j
+    }
+    await fetch("http://localhost:4000/", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data), 
+    })
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ width: "100%", height: 200, display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <button onClick={() => handleClick('* * * * * *', 1)} className='appButton'>1 second</button>
+          <button onClick={() => handleClick('*/2 * * * * *', 2)} className='appButton'>2 second</button>
+          <button onClick={() => handleClick('*/10 * * * * *', 10)} className='appButton'>10 second</button>
+          <button onClick={() => handleClick('*/15 * * * * *', 15)} className='appButton'>15 second</button>
+        </div>
       </header>
     </div>
   );
